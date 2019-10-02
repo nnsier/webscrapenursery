e3eb20580ccf71763f30486a72e431542d6478c4
ecthyr <arreisnahtan@gmail.com>
2019-10-02 19:41:29 -0400

    I think everything is appropriately formatted....

--- convert.js
+++ convert.js
@@ -1,19 +1,33 @@
 const fsPromises = require('fs').promises;
+const fs = require('fs');
 const mongoose = require('mongoose');
 const Plant = require('./mongoose/Plant');
 
 mongoose.connect("mongodb://localhost/nearlyNative", { useNewUrlParser: true });
 
-const file = process.argv[2];
+const argument = process.argv[2];
+let file;
 
-const lireFile = async (file) => {
-  const readFile = await fsPromises.readFile(file, { encoding: 'utf8' });
-  const plantObj = JSON.parse(readFile);
-  console.log(plantObj);
-  await Plant.create(plantObj, (err) => {
-    if (err) { console.log(err) }
-  })
+if (!argument) {
+  file = 'plantArray.js';
+} else {
+  file = argument;
 }
 
 
-// lireFile('plant3.txt');
+const lireFile = async (file) => {
+  const readFile = await fsPromises.readFile(file, { encoding: 'utf8' });
+  const plantArray = readFile.replace(/ \\n         /g, '');
+  fs.appendFile('plantArray2.js', plantArray, (err) => {
+    if (err) throw err;
+    console.log('the data was appended');
+  })
+  // const plantObj = JSON.parse(readFile);
+  // console.log(plantObj);
+  // await Plant.create(plantObj, (err) => {
+  //   if (err) { console.log(err) }
+  // })
+}
+
+
+lireFile(file);
